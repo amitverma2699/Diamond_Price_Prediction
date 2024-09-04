e37ac1f6b97cadccac3e1ce8e44c02e8a9d9b04a
“amitverma2699” <amit99bhama@gmail.com>
2024-09-04 22:37:48 +0530

    update file

--- src/DimondPricePrediction/logger.py
+++ src/DimondPricePrediction/logger.py
@@ -16,3 +16,7 @@ logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[%(asctime)s] %(lineno)d %(name)s -%(levelname)s - %(message)s"
 )
+
+
+if __name__== '__main':
+    logging.info("Here again testing things")
